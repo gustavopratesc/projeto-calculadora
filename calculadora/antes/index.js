b420859bf3067534f8c40207a54c95d5eb9f4bb6
Gustavo Prates Caetano <gustavoprates.c@hotmail.com>
2025-02-11 17:36:37 -0300

    primeiro-teste-calculadora

---
+++ calculadora/antes/index.js
@@ -0,0 +1,18 @@
+
+function outcome() {
+
+    let num1 = Number(document.querySelector('#num-one').value)
+    let num2 = Number(document.querySelector('#num-two').value)
+    let total = 0
+
+    if (document.querySelector("#box1").checked) {
+        total = num1 + num2
+    } else if (document.querySelector("#box2").checked) {
+        total = num1 - num2
+    } else if (document.querySelector("#box3").checked) {
+        total = num1 * num2
+    } else {
+        total = num1 / num2
+    }
+    document.querySelector("#resultArea").innerHTML = 'Result: ' + String(total)
+}
